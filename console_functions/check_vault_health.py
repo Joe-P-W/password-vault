3a7eddf8f1781894997ca2de958d567d0b0bcd8d
Joe Wasden <jpwasden@gmail.com>
2020-01-04 10:12:20 +0000

    Added padding to the password vault

--- console_functions/check_vault_health.py
+++ console_functions/check_vault_health.py
@@ -5,4 +5,11 @@ from crypto_files.encode_vault import encode_vault
 
 def check_vault_health(salt: str):
     if "passwords.json" in os.listdir("vault"):
+        message = "Please use the quit command to exit the vault as not doing this leaves off a layer of encryption"
+        banner = "".join(["!" for i in message])
+        print()
+        print(banner)
+        print(message)
+        print(banner)
+        print()
         encode_vault(salt)
--- crypto_files/encode_vault.py
+++ crypto_files/encode_vault.py
@@ -2,8 +2,19 @@ import json
 import os
 import random
 
+from character_map.maps import maps
 from crypto_files.decode import decode_pass
 from crypto_files.encode import encode_pass
+
+
+def pad(length: int):
+    padding = ""
+    for i in range(length):
+        letter = random.choice(maps)
+        if letter not in ["{", "}"]:
+            padding += letter
+
+    return padding
 
 
 def encode_vault(salt: str):
@@ -22,6 +33,9 @@ def encode_vault(salt: str):
     with open("vault/passwords.json", "r") as vault_file:
         vault = vault_file.read()
 
+    padding1 = pad(random.randint(5, 200))
+    padding2 = pad(random.randint(5, 200))
+    vault = padding1 + vault + padding2
     vault = encode_pass(vault, salt)
 
     with open("vault/passwords.txt", "w") as vault_file:
@@ -45,6 +59,7 @@ def decode_vault(salt: str):
             vault = vault_file.read()
 
     vault = decode_pass(vault, salt)
+    vault = "{" + "}".join(("{".join(vault.split("{")[1:])).split("}")[:-1]) + "}"
 
     with open("vault/passwords.json", "w") as vault_file:
         vault_file.write(vault)
