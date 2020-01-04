a5a0c52a17849dd166b12b260250cd33f6f6f949
Joe Wasden <jpwasden@gmail.com>
2020-01-04 11:01:10 +0000

    Started moving the vault console to its own terminal

--- vault_console.py
+++ vault_console.py
@@ -6,19 +6,27 @@ from console_functions.get_pass_from_vault import get_pass_from_vault
 from console_functions.get_vault_password import get_vault_password
 from crypto_files.encode_vault import encode_vault, decode_vault
 
-salt = get_vault_password()
-check_vault_health(salt)
-decode_vault(salt)
 
-while True:
-    command = input("command? ")
+def main(salt: str):
+    while True:
+        command = input("command? ")
 
-    if command == "quit":
+        if command == "quit":
+            sys.exit()
+
+        elif command == "enter password" or command == "enter pass":
+            enter_password_to_vault()
+
+        elif command == "get password" or command == "get pass":
+            get_pass_from_vault()
+
+
+if __name__ == "__main__":
+    try:
+        salt = get_vault_password()
+        check_vault_health(salt)
+        decode_vault(salt)
+        main(salt)
+    except:
         encode_vault(salt)
-        sys.exit()
-
-    elif command == "enter password" or command == "enter pass":
-        enter_password_to_vault()
-
-    elif command == "get password" or command == "get pass":
-        get_pass_from_vault()
+        raise
